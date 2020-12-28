1817daf14ec7c1987e146bc2378589550a9612e9
chendianbuji <chendianbuji@gmail.com>
2020-12-29 02:34:04 +0800

    update

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,13 @@ module Lib
     ( someFunc
     ) where
 
+import Vulkan.CStruct.Extends
+import Vulkan hiding (allocate)
+import qualified Vulkan.Core10 as Core10
+import qualified Vulkan.Extensions.VK_KHR_swapchain as Swap
+import Vulkan.Extensions.VK_EXT_acquire_xlib_display
+import Vulkan.Utils.ShaderQQ
+import qualified VulkanMemoryAllocator as Vma
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
