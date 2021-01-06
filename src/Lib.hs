7529cdf101fda80db1b3667f1674883540ec7f8d
chendianbuji <chendianbuji@gmail.com>
2021-01-06 12:05:56 +0800

    update

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleInstances #-}
 
-
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE OverloadedLabels #-}
@@ -28,13 +34,18 @@ import Data.Text (Text (..))
 import Data.ByteString (packCString)
 import Data.Traversable (traverse)
 import Data.Bits ((.&.), (.|.), shift, zeroBits)
+import Data.Vector ((!), (!?))
+import qualified Data.Vector as V
+import Data.Set (union)
 
 import Streamly
 import Streamly.Prelude (drain, repeatM)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Resource (ResourceT, runResourceT, allocate, allocate_, release, register)
 import Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
-import Control.Error.Util (hoistMaybe)
+import Control.Monad.Trans.Except (ExceptT, runExceptT)
+import Control.Error.Util (hoistMaybe, failWith)
+import Control.Exception (Exception (..), throw)
 
 someFunc :: IO ()
 someFunc = runResourceT $ do
@@ -44,12 +55,38 @@ someFunc = runResourceT $ do
   withSDL
   window <- withWindow "test" 500 500
   inst <- withInst window
-  surface <- withSurface inst window
-  --liftIO $ print inst
+  surf <- withSurface inst window
+  phys <- getPhysicalDevice inst
+  indices <- findQueueFamilyIndices phys surf
+  liftIO $ print indices
   liftIO $ drain $ asyncly $ constRate 60 $ repeatM $ liftIO $ pure ()
   return undefined
 
 type Managed a = forall m . MonadIO m => ResourceT m a
+
+data AppException
+  = ImageLoadException String
+  | VulkanAllocateMemoryException String
+  | VulkanGraphicsFamilyIndexException
+  | VulkanPresentFamilyIndexException
+  | VulkanLayoutTransitionUnsupport
+  deriving (Show)
+
+instance Exception AppException
+
+class AppMaybe m a where
+  tryWith :: Monad m => a -> Maybe a -> m a
+  tryWithE :: Monad m => AppException -> Maybe a -> m a
+
+instance AppMaybe m a where
+  tryWith :: Monad m => a -> Maybe a -> m a
+  tryWith normal value = runMaybeT (hoistMaybe value) >>= \case
+    Just x -> pure x
+    Nothing -> pure normal
+  tryWithE :: Monad m => AppException -> Maybe a -> m a
+  tryWithE ex value = runExceptT (failWith ex value) >>= \case
+    Right x -> pure x
+    Left e -> throw e
 
 appInfo :: ApplicationInfo
 appInfo = zero { applicationName = Nothing
@@ -80,6 +117,53 @@ withInst window = do
 
 withSurface :: Instance -> SDL.Window -> Managed SurfaceKHR
 withSurface inst window = do
-  (_key, surface) <- allocate (SurfaceKHR <$> SDL.vkCreateSurface window (castPtr $ instanceHandle inst))
+  (_key, surf) <- allocate (SurfaceKHR <$> SDL.vkCreateSurface window (castPtr $ instanceHandle inst))
     (\s -> destroySurfaceKHR inst s Nothing)
-  pure surface
+  pure surf
+
+getPhysicalDevice :: MonadIO m => Instance -> m PhysicalDevice
+getPhysicalDevice inst = do
+  (_, devices) <- enumeratePhysicalDevices inst
+  return $ devices ! 0
+
+data QueueFamilyIndices = QueueFamilyIndices
+  { graphicsFamily :: !Word
+  , presentFamily :: !Word
+  } deriving (Eq, Show, Read)
+
+findQueueFamilyIndices :: MonadIO m => PhysicalDevice -> SurfaceKHR -> m QueueFamilyIndices
+findQueueFamilyIndices pdevice surf = do
+  queueFamilies <- getPhysicalDeviceQueueFamilyProperties pdevice
+  graphicsQueueFamilyIndices <- return $ V.map fst $ V.filter isGraphicsQueue $ V.indexed queueFamilies
+  presentQueueFamilyIndices <- V.map fst <$> V.filterM isPresentQueue (V.indexed queueFamilies)
+  graphicsQueueFamilyIndex <- tryWithE VulkanGraphicsFamilyIndexException (graphicsQueueFamilyIndices !? 0) 
+  -- graphicsQueueFamilyIndex <- (return $ graphicsQueueFamilyIndices !? 0) >>= \case
+  --    Just e -> return e
+  --    Nothing -> throw $ VulkanGraphicsFamilyIndexException
+  presentQueueFamilyIndex <- return $ pickPresentFamilyIndex graphicsQueueFamilyIndex presentQueueFamilyIndices
+  return $ QueueFamilyIndices (fromIntegral graphicsQueueFamilyIndex) (fromIntegral presentQueueFamilyIndex)
+  where
+    isGraphicsQueue :: (Int, QueueFamilyProperties) -> Bool
+    isGraphicsQueue (_i, q) = QUEUE_GRAPHICS_BIT .&. (queueFlags q) /= zeroBits && (queueCount q > 0)
+    isPresentQueue :: MonadIO m => (Int, QueueFamilyProperties) -> m Bool
+    isPresentQueue (i, _q) = getPhysicalDeviceSurfaceSupportKHR pdevice (fromIntegral i) surf
+    pickPresentFamilyIndex :: "graphicsQueueFamilyIndex" ::: Int -> V.Vector Int -> Int
+    pickPresentFamilyIndex gi pis | gi `notElem` pis = case pis !? 0 of
+                                      Just e -> e
+                                      Nothing -> throw $ VulkanPresentFamilyIndexException
+                                  | otherwise = case V.findIndex (/= gi) pis of
+                                      Just e -> e
+                                      Nothing -> gi
+
+findMemoryType :: MonadIO m => PhysicalDevice -> "memoryTypeBits" ::: Word -> MemoryPropertyFlagBits -> m Word
+findMemoryType pdevice typeFilter flagBits = do
+  memProps <- getPhysicalDeviceMemoryProperties pdevice
+  count <- return $ memoryTypeCount memProps
+  memTypes <- return $ memoryTypes memProps
+  (return $ V.findIndex matchp (V.indexed $ V.take (fromIntegral count) memTypes)) >>= \case
+    Just idx -> return $ fromIntegral idx
+    Nothing -> throw $ VulkanAllocateMemoryException "can't allocate video memory"
+  where
+    matchp :: (Int, MemoryType) -> Bool
+    matchp (i, e) = typeFilter .&. (1 `shift` (fromIntegral i)) /= zeroBits && (propertyFlags e) .&. flagBits == flagBits
+
