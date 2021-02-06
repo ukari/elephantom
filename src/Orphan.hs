71b6da63557fa8e018fdfed221c60dca1293f494
chendianbuji <chendianbuji@gmail.com>
2021-02-06 09:46:47 +0800

    refactor, reference https://gitlab.haskell.org/ghc/ghc/-/blob/640a3ece333d1b0d0af8f353c3e1df9dd0cb9ef3/libraries/template-haskell/Language/Haskell/TH/Syntax.hs#L215

--- src/Orphan.hs
+++ src/Orphan.hs
@@ -1,17 +1,15 @@
-{-# LANGUAGE FlexibleInstances #-}
-
 module Orphan
   (
   ) where
 
 --import Data.Semigroup
 import Language.Haskell.TH.Syntax
---import Control.Monad (join)
+-- import Control.Monad (join)
 import Control.Applicative (liftA2)
 
-instance Semigroup (Q [a]) where
+instance Semigroup a => Semigroup (Q a) where
   --(<>) a b = (pure . join =<<) . sequence $ [a, b]
   (<>) = liftA2 (<>)
 
-instance Monoid (Q [a]) where
-  mempty = pure []
+instance Monoid a => Monoid (Q a) where
+  mempty = pure mempty
