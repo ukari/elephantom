d1ae3e964e7e575334d96b7de0d91a358c944b39
chendianbuji <chendianbuji@gmail.com>
2021-02-06 09:48:27 +0800

    add comment

--- src/Orphan.hs
+++ src/Orphan.hs
@@ -7,6 +7,8 @@ import Language.Haskell.TH.Syntax
 -- import Control.Monad (join)
 import Control.Applicative (liftA2)
 
+-- https://gitlab.haskell.org/ghc/ghc/-/blob/640a3ece333d1b0d0af8f353c3e1df9dd0cb9ef3/libraries/template-haskell/Language/Haskell/TH/Syntax.hs#L215
+-- should be removed when use template-haskell >= 2.17.0.0 
 instance Semigroup a => Semigroup (Q a) where
   --(<>) a b = (pure . join =<<) . sequence $ [a, b]
   (<>) = liftA2 (<>)
