10b6644eb6376a9152ec794425acb1715e355022
chendianbuji <chendianbuji@gmail.com>
2021-02-12 01:06:12 +0800

    update spirv module

--- src/SpirV.hs
+++ src/SpirV.hs
@@ -1,24 +1,46 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
 
 module SpirV
   (
   ) where
 
+import Vulkan.Zero (zero)
+import Vulkan.CStruct.Extends (SomeStruct (..))
+import Vulkan.NamedType ((:::))
+import Vulkan.Core10.Shader (ShaderModuleCreateInfo (..), ShaderModule (..))
+import Vulkan.Core10.Enums.Format (Format (..))
+import Vulkan.Core10.Enums.DescriptorType (DescriptorType (..))
+import Vulkan.Core10.DescriptorSet (DescriptorSetLayoutBinding (..), DescriptorSetLayoutCreateInfo (..))
+import Vulkan.Core10.Pipeline (ShaderStageFlagBits (..), PipelineShaderStageCreateInfo (..), PipelineVertexInputStateCreateInfo (..),  VertexInputBindingDescription (..), VertexInputAttributeDescription (..))
 
 import GHC.Generics
-import Data.ByteString.Lazy.Char8 (ByteString, unpack, pack)
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.ByteString.Char8 as B
 import Data.Aeson --(decode, eitherDecode)
 import Data.Vector (Vector)
+import qualified Data.Vector as V
 import System.FilePath ((</>))
 import System.Process.Typed (proc, readProcess)
 import System.IO.Temp (withSystemTempDirectory)
 import Text.InterpolatedString.QM (qnb)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.String (IsString (..))
+import Data.List ((\\), group, sort, sortOn, groupBy)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Function (on)
+import Data.Maybe (fromMaybe)
+import Control.Applicative (liftA2)
+import Control.Monad (join, ap)
 
 data EntryPoint = EntryPoint
   { name :: String
@@ -34,10 +56,10 @@ data Input = Input
 
 instance FromJSON Input where
   parseJSON = withObject "inputs" $ \v -> Input
-        <$> v .: "type"
-        <*> v .: "name"
-        <*> v .: "location"
-        <*> v .:? "array"
+    <$> v .: "type"
+    <*> v .: "name"
+    <*> v .: "location"
+    <*> v .:? "array"
 
 instance ToJSON Input where
   toJSON (Input type' name location array) = object
@@ -53,8 +75,9 @@ instance ToJSON Input where
     <> "array" .= array
     )
 
-data UBO = UBO
-  { set :: Int
+data Ubo = Ubo
+  { name :: String
+  , set :: Int
   , binding :: Int
   , array :: Maybe (Vector Int)
   } deriving (Generic, Show, FromJSON, ToJSON)
@@ -69,11 +92,11 @@ data Texture = Texture
 
 instance FromJSON Texture where
   parseJSON = withObject "textures" $ \v -> Texture
-        <$> v .: "type"
-        <*> v .: "name"
-        <*> v .: "set"
-        <*> v .: "binding"
-        <*> v .:? "array"
+    <$> v .: "type"
+    <*> v .: "name"
+    <*> v .: "set"
+    <*> v .: "binding"
+    <*> v .:? "array"
 
 instance ToJSON Texture where
   toJSON (Texture type' name set binding array) = object
@@ -91,30 +114,161 @@ instance ToJSON Texture where
     <> "array" .= array
     )
 
-
 data Reflection = Reflection
   { entryPoints :: Vector EntryPoint
-  , inputs :: Vector Input
-  , textures :: Vector Texture
-  , ubos :: Vector UBO
+  , inputs :: Maybe (Vector Input)
+  , textures :: Maybe (Vector Texture)
+  , ubos :: Maybe (Vector Ubo)
   } deriving (Generic, Show, FromJSON, ToJSON)
 
 test :: MonadIO m => m (Maybe Reflection)
-test = decode <$> reflect testCode
+test = decode . snd <$> reflect "vert" testVert
 
-reflect :: MonadIO m => String -> m ByteString
-reflect code = liftIO . withSystemTempDirectory "th-spirv" $ \dir -> do
+test2 :: MonadIO m => m (Maybe Reflection)
+test2 = decode . snd <$> reflect "frag" testFrag
+
+-- glslangValidator
+-- -S <stage>  uses specified stage rather than parsing the file extension
+--              choices for <stage> are vert, tesc, tese, geom, frag, or comp
+data ShaderStage
+  = Vert
+  | Frag
+  | Comp
+  | Tesc
+  | Tese
+  | Geom
+
+instance IsString ShaderStage where
+  fromString = \case
+    "vert" -> Vert
+    "frag" -> Frag
+    "comp" -> Comp
+    "tesc" -> Tesc
+    "tese" -> Tese
+    "geom" -> Geom
+    unsupport -> error $ "ShaderStage not support '" <> unsupport <> "'"
+
+instance Show ShaderStage where
+  show = \case
+    Vert -> "vert"
+    Frag -> "frag"
+    Comp -> "comp"
+    Tesc -> "tesc"
+    Tese -> "tese"
+    Geom -> "geom"
+
+convertStage :: ShaderStage -> ShaderStageFlagBits
+convertStage = \case
+  Vert -> SHADER_STAGE_VERTEX_BIT
+  Frag -> SHADER_STAGE_FRAGMENT_BIT
+  Comp -> SHADER_STAGE_COMPUTE_BIT
+  Tesc -> SHADER_STAGE_TESSELLATION_CONTROL_BIT
+  Tese -> SHADER_STAGE_TESSELLATION_EVALUATION_BIT
+  Geom -> SHADER_STAGE_GEOMETRY_BIT
+
+data Shader = Shader
+  { stage :: ShaderStage
+  , code :: B.ByteString
+  }
+
+data ShaderInfo = ShaderInfo
+  { shaderModuleCreateInfo:: ShaderModuleCreateInfo '[]
+  , pipelineShaderStageCreateInfos :: ShaderModule -> Vector (PipelineShaderStageCreateInfo '[])
+  -- , descriptorSetLayoutCreateInfos :: Vector (DescriptorSetLayoutCreateInfo '[])
+  -- , pipelineVertexInputStateCreateInfos :: Maybe (PipelineVertexInputStateCreateInfo '[])
+  }
+
+makeShaderInfo :: (Shader, Reflection) -> ShaderInfo
+makeShaderInfo (Shader {..}, Reflection {..}) = do
+  let shaderModuleCreateInfo = makeShaderModuleCreateInfo code
+  let pipelineShaderStageCreateInfos = makePipelineShaderStageCreateInfos stage entryPoints
+  --let descriptorSetLayoutCreateInfos = makeDescriptorSetLayoutCreateInfos (makeDescriptorSetLayoutBindings stage (fromMaybe [] ubos) (fromMaybe [] textures))
+  --let pipelineVertexInputStateCreateInfos = makePipelineVertexInputStateCreateInfos (fromMaybe [] inputs)
+  ShaderInfo {..}
+
+makeDescriptorInfo :: Vector (Shader, Reflection) -> Vector (DescriptorSetLayoutCreateInfo '[])
+makeDescriptorInfo = makeDescriptorSetLayoutCreateInfos . join . V.map (makeDescriptorSetLayoutBindings . (stage :: Shader -> ShaderStage) . fst <*> fromMaybe [] . ubos . snd <*> fromMaybe [] . textures . snd)
+
+-- makeInputInfo :: Vector (Shader, Reflection) -> Maybe (PipelineVertexInputStateCreateInfo '[])
+-- makeInputInfo
+
+makeShaderModuleCreateInfo :: "code" ::: B.ByteString -> ShaderModuleCreateInfo '[]
+makeShaderModuleCreateInfo code = zero { code = code }
+
+makePipelineShaderStageCreateInfo :: ShaderStage -> ShaderModule -> EntryPoint -> PipelineShaderStageCreateInfo '[]
+makePipelineShaderStageCreateInfo stage shaderModule EntryPoint {..} = zero
+  { stage = convertStage stage
+  , module' = shaderModule
+  , name = B.pack name
+  }
+
+makePipelineShaderStageCreateInfos :: ShaderStage -> Vector EntryPoint -> ShaderModule -> Vector (PipelineShaderStageCreateInfo '[])
+makePipelineShaderStageCreateInfos stage entryPoints shaderModule = V.map (makePipelineShaderStageCreateInfo stage shaderModule) entryPoints
+
+-- https://github.com/KhronosGroup/GLSL/blob/master/extensions/khr/GL_KHR_vulkan_glsl.txt
+-- https://vulkan.lunarg.com/doc/view/1.2.135.0/linux/tutorial/html/08-init_pipeline_layout.html
+makeUboDescriptorSetLayoutBinding :: ShaderStage -> Ubo -> (Int, DescriptorSetLayoutBinding)
+makeUboDescriptorSetLayoutBinding stage Ubo {..} = (set, zero
+  { binding = fromIntegral binding
+  , descriptorType = DESCRIPTOR_TYPE_UNIFORM_BUFFER
+  , descriptorCount = maybe 0 (V.sum . (fromIntegral <$>)) array
+  , stageFlags = convertStage stage
+  })
+
+data TextureDescriptorType
+  = Sampler2D
+
+instance IsString TextureDescriptorType where
+  fromString = \case
+    "sampler2D" -> Sampler2D
+    unsupport -> error $ "TextureDescriptorType not support '" <> unsupport <> "'"
+
+convertTextureDescriptorType :: TextureDescriptorType -> DescriptorType
+convertTextureDescriptorType = \case
+  Sampler2D -> DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER
+
+makeTextureDescriptorSetLayoutBinding :: ShaderStage -> Texture -> (Int, DescriptorSetLayoutBinding)
+makeTextureDescriptorSetLayoutBinding stage Texture {..} = (set, zero
+  { binding = fromIntegral binding
+  , descriptorType = convertTextureDescriptorType . fromString $ type'
+  , descriptorCount = maybe 0 (V.sum . (fromIntegral <$>)) array
+  , stageFlags = convertStage stage
+  })
+
+makeDescriptorSetLayoutBindings :: ShaderStage -> Vector Ubo -> Vector Texture -> Vector (Int, DescriptorSetLayoutBinding)
+makeDescriptorSetLayoutBindings stage ubos textures = do
+  let uboBindings = V.map (makeUboDescriptorSetLayoutBinding stage) ubos
+  let textureBindings = V.map (makeTextureDescriptorSetLayoutBinding stage) textures
+  uboBindings <> textureBindings
+
+makeDescriptorSetLayoutCreateInfos :: Vector (Int, DescriptorSetLayoutBinding) -> V.Vector (DescriptorSetLayoutCreateInfo '[])
+makeDescriptorSetLayoutCreateInfos bindings = do
+  let setSize = V.maximum . (fst <$>) $ bindings :: Int
+  let sets :: Map Int (Vector DescriptorSetLayoutBinding)
+      sets = M.fromList . map (, []) $ [ 0 .. setSize ]
+  let setsMap :: Map Int (Vector DescriptorSetLayoutBinding)
+      setsMap = M.fromList . map (liftA2 (,) (fst . head) (V.fromList . (snd <$>))) . groupBy ((==) `on` fst) . sortOn fst . V.toList $ bindings
+  V.map makeDescriptorSetLayoutCreateInfo . V.fromList . M.elems . M.union sets $ setsMap
+
+makeDescriptorSetLayoutCreateInfo :: Vector DescriptorSetLayoutBinding -> DescriptorSetLayoutCreateInfo '[]
+makeDescriptorSetLayoutCreateInfo bindings = zero { bindings = bindings }
+
+makePipelineVertexInputStateCreateInfos :: Vector Input -> Maybe (PipelineVertexInputStateCreateInfo '[])
+makePipelineVertexInputStateCreateInfos inputs = undefined
+
+reflect :: MonadIO m => ShaderStage -> "code" ::: String -> m (BL.ByteString, BL.ByteString)
+reflect stage code = liftIO . withSystemTempDirectory "th-spirv" $ \dir -> do
   let shader = dir </> "test.vert"
   let spv = dir </> "vert.spv"
   writeFile shader code
   writeFile spv code
-  (exitCode, out, err) <- readProcess . proc "glslangValidator" $ [ "-S", "vert", "-V", shader, "-o", spv]
+  (exitCode, spirv, err) <- readProcess . proc "glslangValidator" $ [ "-S", show stage, "-V", shader, "-o", spv]
   --print out
-  (exitCode, out, err) <- readProcess . proc "spirv-cross" $ [spv, "--vulkan-semantics", "--reflect"]
-  pure out
+  (exitCode, reflection, err) <- readProcess . proc "spirv-cross" $ [spv, "--vulkan-semantics", "--reflect"]
+  pure (spirv, reflection)
 
-testCode :: String
-testCode = [qnb|
+testVert :: String
+testVert = [qnb|
   #version 450
   #extension GL_ARB_separate_shader_objects : enable
   const int a = 1;
@@ -140,5 +294,24 @@ testCode = [qnb|
     gl_Position = ubo[3].proj * ubo[3].view * ubo[3].model * vec4(position, 0.0, 1.0);
     fragColor = color;
     fragTexCoord = texCoord;
+  }
+  |]
+
+testFrag :: String
+testFrag = [qnb|
+  #version 450
+
+  #extension GL_ARB_separate_shader_objects : enable
+
+  layout(binding = 1) uniform sampler2D texSampler[2];
+  layout(set = 3, binding = 1) uniform sampler2D texSampler2;
+
+  layout(location = 0) in vec4 fragColor;
+  layout(location = 1) in vec2 fragTexCoord;
+
+  layout(location = 0) out vec4 outColor;
+
+  void main() {
+    outColor = texture(texSampler[0], fragTexCoord); // for image use texSampler, for shape created by rasterfic use fragColor
   }
   |]
