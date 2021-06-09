d8fbca99d7a66f89d3202076116f2770ef1fd938
chendianbuji <chendianbuji@gmail.com>
2021-06-09 22:58:31 +0800

    update

--- src/Conv.hs
+++ src/Conv.hs
@@ -1,15 +1,22 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+--{-# LANGUAGE OverloadedLists #-}
 
-module Conv () where
+module Conv
+  (
+  ) where
 
+import Data.Massiv.Array
+import Data.Massiv.Array.Numeric ((!*!))
 
 import Numeric.AD
 
-
 test :: IO ()
 test = do
-  
+  let mt = singleton 1 :: Array U Ix1 Int
+  let mt0 = fromLists' Par [[0..3], [4..7]] :: Array U Ix2 Int
+  let mt1 = fromLists' Par [[0,1], [2,3], [4,5],[6,7]] :: Array U Ix2 Int
+  print mt0
+  print mt1
+  print $ mt0 !><! mt1
   pure ()
