641f63ce9f1715b873e8b95d56bfbb6bb572b484
chendianbuji <chendianbuji@gmail.com>
2021-06-11 21:16:23 +0800

    update

--- src/Conv.hs
+++ src/Conv.hs
@@ -24,7 +24,6 @@ test = do
   print $ mt0 !><! mt1
   let lm0 = fromLists' Par [[0 .. 9999], [9999 .. 0]] :: Array U Ix2 Int
   pure $ mt1 !><! lm0
-
   pure ()
 
 --foo = [0..5] :: V 6 Int
@@ -36,3 +35,13 @@ lin = do
   print $ l2 !*! foo
   pure ()
 
+test2 :: IO ()
+test2 = do
+  let a0 = 1...64 :: Array D Ix1 Int
+  a2 <- resizeM (Sz (8 :. 8)) a0
+  let a3 = compute a2 :: Array U Ix2 Int
+  print $ getComp a3
+  let st = makeStencil (Sz (3 :. 3)) (1 :. 0) (\get -> get (0 :. 2))
+  print a0
+  print $ computeAs U $ (+0) <$> mapStencil Wrap st a3
+  pure ()
