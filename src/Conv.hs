4cb33f2145daed49af3c3b13f9f6ca13aa417db9
chendianbuji <chendianbuji@gmail.com>
2021-06-14 05:25:37 +0800

    update

--- src/Conv.hs
+++ src/Conv.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedLists #-}
+
 
 module Conv
   (
   ) where
 
 import Data.Time (getCurrentTime, diffUTCTime)
-import Data.Massiv.Array hiding ((!*!))
+import Data.Vector.Unboxed (iterateN, generate, unfoldrExactN)
+import qualified Data.Vector.Unboxed as VU
+import Data.Massiv.Array hiding ((!*!), R, iterateN, generate, toList, fromList)
 import Data.Massiv.Array.Manifest.Vector (VRepr, ARepr, toVector)
+import Numeric.LinearAlgebra.Data (reshape)
+import Numeric.LinearAlgebra.Static (R (..), Domain(..), Sized (..), (===), vec4, row, tr, vector)
+
 --import Data.Massiv.Array.Numeric ((!*!))
 --import Linear.V
 --import Linear
@@ -17,10 +25,12 @@ import Control.Lens.Setter (ASetter')
 import Numeric.AD
 import System.Random
 
-import qualified Data.Vector.Unboxed as VU
+
 import qualified Data.Vector.Generic as VG
 import Data.Bifunctor (first)
 import Control.Applicative (liftA2)
+
+
 
 test :: IO ()
 test = do
@@ -47,15 +57,42 @@ test2 = do
 
 testdot :: IO ()
 testdot = do
+  tic0 <- getCurrentTime
   let rd = mkStdGen 2
-  let a0 = randomArray rd split (random) Seq (Sz 1000000) :: Array DL (Ix 1) Double
+  let a0 = randomArray rd split (random) (ParN 16) (Sz 10000000) :: Array DL (Ix 1) Double
   --print a0
-  let a1 = compute a0 :: Array U Ix1 Double
+  let a1 = computeP a0 :: Array U Ix1 Double
+  toc0 <- getCurrentTime
+  print $ diffUTCTime toc0 tic0
   --let v0 = toVector a1 :: VU.Vector Double
   tic <- getCurrentTime
-  v <- dotM a1 a1
+  let v = a1 !.! a1
   toc <- getCurrentTime
   print v
   print $ diffUTCTime toc tic
 
   pure ()
+
+testh ::IO ()
+testh = do
+  let a = row (vec4 1 2 3 4)
+          ===
+          row (vec4 5 6 7 8)
+  print $ tr a
+  let rd = mkStdGen 2
+  let rdv = unfoldrExactN @Float 10 random rd
+  tic0 <- getCurrentTime
+  --let v = vector $ VU.toList rdv :: R 10
+  let v = vector (VU.toList $ unfoldrExactN 10000000 random (mkStdGen 2) :: [Double]) :: R 10000000
+  toc0 <- getCurrentTime
+  print $ diffUTCTime toc0 tic0
+  tic <- getCurrentTime
+  let b = v `dot` v
+  toc <- getCurrentTime
+  print b
+  print $ diffUTCTime toc tic
+
+
+  pure ()
+
+foob = random @Float $ snd $ (random @Float) $ mkStdGen 2
