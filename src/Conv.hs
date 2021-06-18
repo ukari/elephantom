08f5f97d8bb1fbf1a8d729ddd403e68a9e76177b
chendianbuji <chendianbuji@gmail.com>
2021-06-18 12:39:31 +0800

    update

--- src/Conv.hs
+++ src/Conv.hs
@@ -23,6 +23,15 @@ import Data.Bifunctor (first)
 import Control.Applicative (liftA2)
 import Control.Scheduler (getCompWorkers)
 
+wx :: IO ()
+wx = do
+  let w = fromLists' Seq [[0,0.1,0.2,0.3], [0.4,0.5,0.6,0.7]] :: Array U Ix2 Double
+  let x = fromLists' Seq [[0, 1,2,3,4], [5,6,7,8,9], [0,1,2,3,4], [5,6,7,8,9]] :: Array U Ix2 Double
+  print w
+  print x
+  print $ w !><! x
+  pure ()
+
 test :: IO ()
 test = do
   let mt = singleton 1 :: Array U Ix1 Int
@@ -72,3 +81,82 @@ count f = do
   toc <- getCurrentTime
   print $ diffUTCTime toc tic
 
+-- x1 = [1,2]
+-- x2 = [100,200]
+-- u = [50.5,101]
+-- x1' = [-49.5, -99]
+-- x2' = [49.5, 99]
+
+--
+--
+--
+-- (x1^2 - x2^2)/(x1^2 + x2^2)
+--
+-- w1 * x1 + b
+-- w2 * x2 + b
+-- w3 * x3 + b
+-- ...
+-- wm * xm + b
+
+-- W*X
+
+-- Wl = 0.5 /10
+       -- 0.6
+       -- 0.1
+       -- 0.3
+-- X = [2,3,4,5]
+-- W*X = 1    1.5  2  2.5    1.5^0.5/4^0.5   y = 2  al = (E(yhat i - y)^2)^0.5
+         -- 1.2  1.8
+         -- 0.2
+         -- 0.6
+
+
+
+-- theta (WX) =
+
+-- X -> W1X = A1 -> W2*A1 = A2
+
+-- [m特征值 x n样本数]  [mx1]
+
+
+
+-- 样本数 m
+-- 特征数 n
+-- x n维向量
+-- X n行xm列
+-- Y 1行xm列
+-- w n维向量
+-- w^t w的转置
+-- b 实数
+-- z 设z为w^t * x + b
+-- yhat 单样本预测值 yhat = sigma(w^t * x + b) = sigma(z)
+-- sigma(z) = 1 / (1 + e ^ (-z))
+-- loss(yhat, y) = - (y * log(yhat) + (1 - y) * log(1 - yhat)) 交叉熵损失函数 Cross Entropy Loss
+-- J(w,b) = (1/m) * sum(loss(yhat(i), y(i))) 成本函数 cost function
+-- https://towardsdatascience.com/using-the-right-dimensions-for-your-neural-network-2d864824d0df
+
+data Input = Input Int deriving (Show)
+
+data Layer = FullyConnected (Array U Ix2 Double) Double deriving (Show)
+
+mkWeight :: StdGen -> Int -> Int -> Array U Ix2 Double
+mkWeight rseed featureNum sampleNum = computeAs U . resize' (Sz (featureNum :. sampleNum)) . randomArray rseed split random Par . Sz $ featureNum * sampleNum
+
+mkFull :: StdGen -> Int -> Int -> Layer
+mkFull rseed featureNum sampleNum = FullyConnected w b where
+  b :: Double
+  nrseed :: StdGen
+  (b, nrseed) = random rseed
+  w :: Array U Ix2 Double
+  w = computeAs U . resize' (Sz (featureNum :. sampleNum)) . randomArray nrseed split random Par . Sz $ featureNum * sampleNum
+
+testimnn :: IO ()
+testimnn = do
+  --let input = Input 2
+  let layer1 = mkFull (mkStdGen 0) 3 2
+  let layer2 = mkFull (mkStdGen 1) 2 3
+  let layer3 = mkFull (mkStdGen 2) 1 2
+  print layer1
+  print layer2
+  print layer3
+  pure ()
