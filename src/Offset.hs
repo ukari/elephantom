24180e6d94d48d93154016ccb037e5f0dcead46a
chendianbuji <chendianbuji@gmail.com>
2022-01-23 16:57:55 +0800

    update dependencies

--- src/Offset.hs
+++ src/Offset.hs
@@ -4,5 +4,5 @@ module Offset
   ( module Foreign.Storable.Offset
   ) where
 
-import "offset" Foreign.Storable.Offset
---import Foreign.Storable.Offset
+--import "offset" Foreign.Storable.Offset
+import Foreign.Storable.Offset
